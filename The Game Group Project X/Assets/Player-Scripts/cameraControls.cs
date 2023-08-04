3f1a76f3a9005c28c22fa039317cfb95da459822
CaptainShinyPants <GAVegaSalcedo@student.fullsail.edu>
2023-08-04 00:10:43 -0400

    Implemented Player for testing in other scenes
    Based on the examples done in class this is a more simplified version of the player so other members have something to work within their scenes. I'll add more later today.

---
+++ The Game Group Project X/Assets/Player-Scripts/cameraControls.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraControls : MonoBehaviour
+{
+    [SerializeField] int sensitivity;
+
+    [SerializeField] int lockVertMin;
+    [SerializeField] int lockVertMax;
+
+    float xRotation;
+
+    void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+
+    void Update()
+    {
+        float mouseY = Input.GetAxis("Mouse Y") * Time.deltaTime * sensitivity;
+        float mouseX = Input.GetAxis("Mouse X") * Time.deltaTime * sensitivity;
+
+        xRotation = Mathf.Clamp(xRotation, lockVertMin, lockVertMax);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+
+        transform.parent.Rotate(Vector3.up * mouseX);
+    }
+}
