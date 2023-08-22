ce02bce26bc69f394f4f191ba725ccd7e9fd2c72
CaptainShinyPants <GAVegaSalcedo@student.fullsail.edu>
2023-08-21 20:11:26 -0400

    Revert "Revert "quick reload speed up""
    This reverts commit db5dca9da195141cfb1011c43eec80af854474d8.

--- The Game Group Project X/Assets/Scripts/Player-Scripts/GunhealthScripts/GUNtemp.cs
+++ The Game Group Project X/Assets/Scripts/Player-Scripts/GunhealthScripts/GUNtemp.cs
@@ -12,7 +12,7 @@ public class GUNtemp : ScriptableObject
     public int currentAmmo;
     public int weaponClipSize;
     public int maxAmmo;
-    public int ReloadTime;
+    [Range(1, 10)] public float ReloadTime;
 
     public string weaponDifferentiator;
 
