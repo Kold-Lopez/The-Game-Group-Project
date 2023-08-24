32c5b136befe45611bdbdd9abca20f8211f3d9fe
Prysmil <thompcaedan@gmail.com>
2023-08-23 20:14:58 -0400

    Boostybars fix part 2
    i hope this is the last fix needed

--- The Game Group Project X/Assets/Scripts/Player-Scripts/GunhealthScripts/BoostBars.cs
+++ The Game Group Project X/Assets/Scripts/Player-Scripts/GunhealthScripts/BoostBars.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 public class BoostBars : MonoBehaviour
 {
     [Header("----- TheBoostyBarObject -----")]
+    [SerializeField] SphereCollider sphereCollider;
     [SerializeField] GameObject BoostyBars;
     [Header("----- StatChangeMultiplier -----")]
-    [Range(1, 10)][SerializeField] float SpeedIncrease;
     [Range(1, 10)][SerializeField] float SprintSpeedIncrease;
     [Range(30, 120)][SerializeField] int effectDuration;
+    private bool boostyBarUsed;
 
     // Start is called before the first frame update
 
@@ -24,9 +25,12 @@ public class BoostBars : MonoBehaviour
 
     IEnumerator chocolateyBars()
     {
+        boostyBarUsed = true;
+        sphereCollider.enabled = false;
         BoostyBars.SetActive(false);
-       gameManager.instance.playerScript.makeManSpeedy(SpeedIncrease, SprintSpeedIncrease, 0);
+       gameManager.instance.playerScript.makeManSpeedy(0, SprintSpeedIncrease, 0);
        yield return new WaitForSeconds(effectDuration);
-        gameManager.instance.playerScript.makeManSpeedy(-SpeedIncrease, -SprintSpeedIncrease, 0);
+        gameManager.instance.playerScript.makeManSpeedy(0, -SprintSpeedIncrease, 0);
+        boostyBarUsed = false;
     }
 }
